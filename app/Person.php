3121cc087869e19c1ffa88251db46ca3aa06ac17
lospatitos <go@lospatitos-mac.local>
2020-04-11 19:09:50 +0900

    6-2 検索とスコープ

--- app/Person.php
+++ app/Person.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use App\Scopes\ScopePerson;
+use BadFunctionCallException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
@@ -9,5 +12,22 @@ class Person extends Model
     public function getData()
     {
         return $this->id . ': ' . $this->name . '(' . $this->age . ')';
+    }
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new ScopePerson);
     }
 }
